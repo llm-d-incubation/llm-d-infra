9ed6565dda2a37c04a47d8abaa4a7549003046af
Robert Shaw <rshaw@neuralmagic.com>
2025-08-27 02:11:01 -0400

    updated
    Signed-off-by: Robert Shaw <rshaw@neuralmagic.com>

--- quickstart/patch-istio-deployment.sh
+++ quickstart/patch-istio-deployment.sh
@@ -1,6 +1,5 @@
-export DEP=infra-wide-ep-inference-gateway-istio
-export NS=llm-d-wide-ep
+export DEP=infra-wide-ep-pd-inference-gateway-istio
+export NS=llm-d-wide-ep-pd
 
 kubectl patch deploy "$DEP" -n "$NS" --type='merge' \
   -p='{"spec":{"template":{"spec":{"tolerations":[{"key":"nvidia.com/gpu","operator":"Exists","effect":"NoSchedule"}]}}}}'
-  
